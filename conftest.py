9080f51352a9865b00ab51e7247547bca938af4b
elvsab <elvsab@bk.ru>
2023-12-20 23:40:30 +0600

    Update conftest.py
    added configuration for Mozilla

--- conftest.py
+++ conftest.py
@@ -1,20 +1,25 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def driver():
-    driver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service)
+def driver(request):
+    browser_name = request.config.getoption("driver")
+    if browser_name == "chrome":
+        driver_service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=driver_service)
+    elif browser_name == "firefox":
+        driver_service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=driver_service)
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}")
+
     driver.maximize_window()
     driver.get("https://dima731515.github.io/hakaton/html/")
-    
+
     yield driver
     driver.quit()
-
-    
